b1311e00d1b34fa2473d685ef9929cb96589e391
vyadav <vyadav@C5-VINOD.ad.utb.cz>
2021-02-19 13:15:46 +0100

    With new Student class

---
+++ JavaTutorial/src/com/java/tutorial/Student1.java
@@ -0,0 +1,11 @@
+package com.java.tutorial;
+
+public class Student1 
+	{
+		int id;
+		String name;
+		public static void main(String[] args) {
+			Student1 s1=new Student1();
+			System.out.println(s1.id+" "+s1.name);
+		}
+	}
