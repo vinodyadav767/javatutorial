12bda54872cce1f90d356f38a7d061663938f333
vyadav <vyadav@C5-VINOD.ad.utb.cz>
2021-02-20 12:05:26 +0100

    With some changes

--- JavaTutorial/src/com/java/tutorial/P1.java
+++ JavaTutorial/src/com/java/tutorial/P1.java
@@ -1,5 +1,6 @@
 package com.java.tutorial;
 
+// Hello world
 public class P1 {
 	public static void main(String[] args) {
 		System.out.println("Hello in Java World");
--- JavaTutorial/src/com/java/tutorial/Student1.java
+++ JavaTutorial/src/com/java/tutorial/Student1.java
@@ -1,11 +1,12 @@
 package com.java.tutorial;
 
-public class Student1 
-	{
-		int id;
-		String name;
-		public static void main(String[] args) {
-			Student1 s1=new Student1();
-			System.out.println(s1.id+" "+s1.name);
-		}
+//Object and Class Example: main within the class
+public class Student1 {
+	int id;
+	String name;
+
+	public static void main(String[] args) {
+		Student1 s1 = new Student1();
+		System.out.println(s1.id + " " + s1.name);
 	}
+}
